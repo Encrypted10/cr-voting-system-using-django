a117c9b6e20f0cb79299b714867259c41ccebc43
encypted10 <kmarsalan.com>
2025-08-28 19:52:37 +0530

    remove material admin

--- voting_site/settings.py
+++ voting_site/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    'material',
-     'material.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
